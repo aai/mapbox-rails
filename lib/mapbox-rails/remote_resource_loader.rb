0850a926feef8b20542897353cbd51a9599a8682
Joshua Kovach <kovach.jc@gmail.com>
2015-07-07 13:59:00 -0400

    Remove unnecessary .css file extension

--- lib/mapbox-rails/remote_resource_loader.rb
+++ lib/mapbox-rails/remote_resource_loader.rb
@@ -21,14 +21,14 @@ class RemoteResourceLoader < Thor
   def convert
     self.destination_root = 'app/assets'
     inside destination_root do
-      run('sass-convert -F css -T scss stylesheets/mapbox.css stylesheets/mapbox.css.scss')
-      gsub_file 'stylesheets/mapbox.css.scss', 'url(images/layers.png)',          "image-url('layers.png')"
-      gsub_file 'stylesheets/mapbox.css.scss', 'url(images/layers-2x.png)',       "image-url('layers-2x.png')"
-      gsub_file 'stylesheets/mapbox.css.scss', 'url(images/marker-icon.png)',     "image-url('marker-icon.png')"
-      gsub_file 'stylesheets/mapbox.css.scss', 'url(images/marker-icon-2x.png)',  "image-url('marker-icon-2x.png')"
-      gsub_file 'stylesheets/mapbox.css.scss', 'url(images/marker-shadow.png)',   "image-url('marker-shadow.png')"
-      gsub_file 'stylesheets/mapbox.css.scss', 'url(images/icons-000000@2x.png)', "image-url('icons-000000@2x.png')"
-      gsub_file 'stylesheets/mapbox.css.scss', 'url(images/icons-ffffff@2x.png)', "image-url('icons-ffffff@2x.png')"
+      run('sass-convert -F css -T scss stylesheets/mapbox.css stylesheets/mapbox.scss')
+      gsub_file 'stylesheets/mapbox.scss', 'url(images/layers.png)',          "image-url('layers.png')"
+      gsub_file 'stylesheets/mapbox.scss', 'url(images/layers-2x.png)',       "image-url('layers-2x.png')"
+      gsub_file 'stylesheets/mapbox.scss', 'url(images/marker-icon.png)',     "image-url('marker-icon.png')"
+      gsub_file 'stylesheets/mapbox.scss', 'url(images/marker-icon-2x.png)',  "image-url('marker-icon-2x.png')"
+      gsub_file 'stylesheets/mapbox.scss', 'url(images/marker-shadow.png)',   "image-url('marker-shadow.png')"
+      gsub_file 'stylesheets/mapbox.scss', 'url(images/icons-000000@2x.png)', "image-url('icons-000000@2x.png')"
+      gsub_file 'stylesheets/mapbox.scss', 'url(images/icons-ffffff@2x.png)', "image-url('icons-ffffff@2x.png')"
     end
   end
 
